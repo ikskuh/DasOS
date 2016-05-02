782fddf606b9020f70ba4add6b7751f48767e3aa
Felix Queißner <git@mq32.de>
2016-05-03 00:43:14 +0200

    Adds stub class for physical memory managment.

---
+++ prototypes/base/include/pmm.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stddef.h>
+
+/**
+ * Physical memory management tool.
+ */
+class PMM
+{
+private:
+  PMM() = delete;
+public:
+  /**
+   * Marks a page as occupied by external memory management.
+   * @returns true if the page was previously free, false if it was already allocated.
+   */
+  static bool markOccupied(void *page);
+
+  /**
+   * Allocates a single page.
+   */
+  static void* alloc();
+
+  /**
+   * Frees a given page by pointer.
+   */
+  static void free(void *page);
+};
---
+++ prototypes/base/src/pmm.cpp
@@ -0,0 +1,16 @@
+#include "pmm.hpp"
+
+bool PMM::markOccupied(void *page)
+{
+  return false;
+}
+
+void *PMM::alloc()
+{
+  return nullptr;
+}
+
+void PMM::free(void *page)
+{
+  
+}
