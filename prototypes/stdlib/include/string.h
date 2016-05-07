585cb336806c0960ae5f8d71ff8ad3088c8a67d5
Felix Queißner <git@mq32.de>
2016-05-07 19:56:13 +0200

    Adds extern "C" to header file.

--- prototypes/stdlib/include/string.h
+++ prototypes/stdlib/include/string.h
@@ -2,6 +2,15 @@
 
 #include <stddef.h>
 
+#if defined(__cplusplus)
+extern "C" { 
+#endif
+
 void * memcpy ( void * destination, const void * source, size_t num );
 
 void * memset ( void * ptr, int value, size_t num );
+
+
+#if defined(__cplusplus)
+}
+#endif
