0b7a30e42d76a3280eb80f8d7ff66c860bdd2c59
Felix Queißner <git@mq32.de>
2016-05-22 01:37:41 +0200

    Removes old ASM code.

--- prototypes/supervm-asm/testcode.asm
+++ prototypes/supervm-asm/testcode.asm
@@ -46,27 +46,3 @@ print_str_end_loop:
 	bpget ; leave function
 	spset ; by restoring parent base pointer
 	jmpi  ; and jumping back.
-	
-;	load 0
-;	load 1
-;	load 2
-;	load 3
-	
-;	store 67
-;	store 66
-;	store 65
-;	store 64
-	
-
-
-
-
-;	push 0 ; Some comment
-;_loop:
-;	dup
-;	[i0:arg] sub 10 [f:yes] [r:discard]
-;	[ex(z)=1] jmp @_end
-;	[i0:arg] add 1
-;	jmp @_loop
-;_end:
-;	syscall [ci:0]; Exit
