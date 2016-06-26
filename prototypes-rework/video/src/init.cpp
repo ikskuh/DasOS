99a53601b7d9d87121af0e074c5fe9f71f253290
Felix Queißner <git@mq32.de>
2016-06-26 16:13:21 +0200

    SerialPort driver in video.ker

--- prototypes-rework/video/src/init.cpp
+++ prototypes-rework/video/src/init.cpp
@@ -1,15 +1,17 @@
 #include <stdint.h>
 
 #include <multiboot.hpp>
-#include <io.hpp>
 
 #include <driver/video.hpp>
+#include <driver/serial.hpp>
 
 extern "C" void init(multiboot::Structure const & data)
 {
-	write_com(0x3F8, 'H');
-	write_com(0x3F8, 'i');
-	write_com(0x3F8, '\n');
+	SerialPort serial(SERIAL_COM1);
+
+	serial.write('H');
+	serial.write('i');
+	serial.write('\n');
 	
 	VideoScreen video(*data.vbe.modeInfo);
 	for(uint32_t y = 0; y < video.height(); y++)
@@ -19,11 +21,6 @@ extern "C" void init(multiboot::Structure const & data)
 			video(x,y) = COLOR(255, 0, 0);
 		}
 	}
-	
-	write_com(0x3F8, 'B');
-	write_com(0x3F8, 'y');
-	write_com(0x3F8, 'e');
-	write_com(0x3F8, '\n');
 	
 	while(true);
 }
