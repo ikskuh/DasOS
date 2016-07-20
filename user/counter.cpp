a7c83fa7464ada5e8448dac8af0341838ba708cb
Felix Queißner <git@mq32.de>
2016-07-20 08:22:16 +0200

    Adds primitive keyboard driver.

--- user/counter.cpp
+++ user/counter.cpp
@@ -2,8 +2,6 @@
 #include <stdint.h>
 #include "console.hpp"
 
-uint16_t *video = (uint16_t*)0xB8000;
-
 extern "C" void main()
 {
 	Console::main << FColor(Color::LightRed) << "Hello World!\n" << FColor(Color::White);
