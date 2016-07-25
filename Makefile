11662cc2922e57a304b26daf86132b3fe75b6c48
Felix Queißner <git@mq32.de>
2016-07-26 01:10:32 +0200

    Adds "game" demo with simple sprite rendering.

--- Makefile
+++ Makefile
@@ -7,16 +7,19 @@ kernel: ./kernel/
 user: ./user/
 	$(MAKE) -C $@ -B
 
+.PHONY: boot.img
 boot.img:
 	mformat -C -f 1440 -v VIDEO -i boot.img ::
 	mcopy -i boot.img \
 		kernel/DasOS \
 		user/demo/demo \
+		user/game/game \
 		syslinux.cfg \
 		/boot/syslinux/libcom32.c32 \
 		/boot/syslinux/libutil.c32 \
 		/boot/syslinux/menu.c32 \
 		/boot/syslinux/mboot.c32 \
+		/boot/syslinux/poweroff.c32 \
 		::
 	mmd -i boot.img ::/resource
 	mcopy -i boot.img resources/* ::/resource/
---
+++ user/game/src/main.c
@@ -0,0 +1,53 @@
+#include <stdint.h>
+#include "syscalls.h"
+
+typedef struct {
+	uint32_t width, height, stride; // stride is in pixels, not bytes!
+	const color_t * pixels;
+} sprite_t;
+
+void blit_sprite(const sprite_t * sprite, int x, int y)
+{
+	for(uint32_t dy = 0; dy < sprite->height; dy++) {
+		for(uint32_t dx = 0; dx < sprite->width; dx++) {
+			color_t color = sprite->pixels[sprite->stride * dy + dx];
+			if(color.a >= 128) {
+				video_setpixel(x + dx, y + dy, color);
+			}
+		}
+	}
+}
+
+extern color_t iso_tree[];
+extern color_t iso_gras[];
+
+void main()
+{
+	color_t background = { 32, 64, 192};
+	video_clear(background);
+	
+	// TODO: Implement game
+	
+	sprite_t gras = { 32, 32, 32, iso_gras };
+	sprite_t tree = { 32, 32, 32, iso_tree };
+	
+	int lh = 19;
+	for(int i = 0; i < 3; i++) {
+		blit_sprite(&gras, 16, 16 + lh * i);
+		blit_sprite(&gras, 48, 16 + lh * i);
+		blit_sprite(&gras, 80, 16 + lh * i);
+		blit_sprite(&gras, 112, 16 + lh * i);
+		blit_sprite(&gras, 32, 25 + lh * i);
+		blit_sprite(&gras, 64, 25 + lh * i);
+		blit_sprite(&gras, 96, 25 + lh * i);
+	}
+	blit_sprite(&gras, 48, 72);
+	blit_sprite(&gras, 80, 72);
+	blit_sprite(&gras, 64, 80);
+	
+	blit_sprite(&tree, 64, 25 + lh * 2);
+	
+	video_swap();
+	
+	while(1);
+}
